771f3bdc72831523ebbda9bff0381365301cb78a
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-04-17 11:41:51 -0400

    wrote helper function to get posts

--- src/api/users.js
+++ src/api/users.js
@@ -27,6 +27,7 @@ export const getPosts = async () => {
 	try {
 		const response = await fetch(`${BASE_URL}/posts`);
 		const result = await response.json();
+		console.log(result);
 		return result.data.posts;
 	} catch (error) {
 		console.error("ERROR");
