e2338ab7078e9423a152b58b39ec604b3c9178b4
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-04-17 10:17:52 -0400

    starting point

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { registerUser } from "../api/users";
+export default function RegisterForm() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			const result = await registerUser(username, password);
+			console.log("Result in component: ", result);
+			setToken(result.data.token);
+			localStorage.setItem("token", result.data.token);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="username"
+					placeholder="username"
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="text"
+					name="password"
+					placeholder="password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+			</form>
+		</div>
+	);
+}
