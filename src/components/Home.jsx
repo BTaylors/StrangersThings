cbe3fbde6299e59ba05bc4418d60a547149c71a8
Brandon Taylor <bmt1994.tlh@gmail.com>
2023-04-20 10:45:04 -0400

    sync with partner

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AllPosts from "./AllPosts";
+
+export default function Home() {
+	return (
+		<div className="home">
+			<h1 className="home-header" style={{ textDecoration: "underline" }}>
+				All Posts
+			</h1>
+			<AllPosts />
+		</div>
+	);
+}
